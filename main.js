5c73bcb131bafc5dde280f84b8bdf6f2d119a128
codepilot2010 <93812156+codepilot2010@users.noreply.github.com>
2023-07-13 19:22:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 song="";
 leftwristx=0;
 leftwristy=0;
-rightwristx=0;
-righttwristy=0;
+right_wrist_x=0;
+right_wrist_y=0;
 scoreleftwrist=0;
 
 
@@ -27,9 +27,9 @@ function gotPoses(results)
         leftwristy=results[0].pose.leftWrist.y;
         console.log("left wrist x = "+leftwristx+"left wrist y = "+leftwristy);
 
-        rightwristx=results[0].pose.rightWrist.x;
-        rightwristy=results[0].pose.rightWrist.y;
-        console.log("right wrist x = "+rightwristx+"right wrist y = "+rightwristy);
+        right_wrist_x=results[0].pose.rightWrist.x;
+        right_wrist_y=results[0].pose.rightWrist.y;
+        console.log("right wrist x = "+right_wrist_x+"right wrist y = "+right_wrist_y);
     }
 }
 
@@ -43,6 +43,38 @@ function draw()
     image(video,0,0,600,500);   
     fill("#FF0000");
     stroke("#FF0000");
+    circle(right_wrist_x,right_wrist_y,20);
+    if (right_wrist_y>0&&right_wrist_y<=100)
+    {
+        document.getElementById("speed").innerHTML="speed = 0.5x";
+        song.rate(0.5);
+    }
+
+    else if (right_wrist_y>100&&right_wrist_y<=200)
+    {
+        document.getElementById("speed").innerHTML="speed = 1x";
+        song.rate(1);
+    }
+
+    else if (right_wrist_y>200&&right_wrist_y<=300)
+    {
+        document.getElementById("speed").innerHTML="speed = 1.5x";
+        song.rate(1.5);
+    }
+
+    else if (right_wrist_y>300&&right_wrist_y<=400)
+    {
+        document.getElementById("speed").innerHTML="speed = 2x";
+        song.rate(2);
+    }
+
+    else if (right_wrist_y>400&&right_wrist_y<=500)
+    {
+        document.getElementById("speed").innerHTML="speed = 2.5";
+        song.rate(2.5);
+    }
+
+
     if(scoreleftwrist>0.2)
     {
         circle(leftwristx,leftwristy,20);
